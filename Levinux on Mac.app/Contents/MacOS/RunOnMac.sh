d0970887a697227c78915772d29832fae90459f3
Mike Levin <miklevin@gmail.com>
2014-07-14 17:54:21 -0400

    Added a couple new .dylib files and a new bios for Mac

--- Levinux on Mac.app/Contents/MacOS/RunOnMac.sh
+++ Levinux on Mac.app/Contents/MacOS/RunOnMac.sh
@@ -5,7 +5,7 @@ osascript -e 'tell app "Terminal"
     set myFolder to parent of (path to me) as alias
     end
     set myPosixFolder to POSIX path of myFolder
-    do script "DYLD_LIBRARY_PATH=" & myPosixFolder & "Levinux\\ on\\ Mac.app/Contents/MacOS && export DYLD_LIBRARY_PATH && cd " & myPosixFolder & "Levinux\\ on\\ Mac.app/Contents/MacOS && ./i386-softmmu -curses -kernel vmlinuz -initrd microcore.gz -hda home.qcow -hdb opt.qcow -hdc tce.qcow -L ./macbios/ -tftp ../../../Reset/Server -redir tcp:2222::22 -redir tcp:8080::80 -redir tcp:8888::81 -append \"quiet noautologin home=sda1 opt=sdb1 tce=sdc1\" 2>../../../Reset/MacErrorLog.txti && exit"
+    do script "DYLD_LIBRARY_PATH=" & myPosixFolder & "Levinux\\ on\\ Mac.app/Contents/MacOS && export DYLD_LIBRARY_PATH && cd " & myPosixFolder & "Levinux\\ on\\ Mac.app/Contents/MacOS && ./i386-softmmu -curses -kernel vmlinuz -initrd microcore.gz -hda home.qcow -hdb opt.qcow -hdc tce.qcow -L ./macbios/ -tftp ../../../Reset/Server -redir tcp:2222::22 -redir tcp:8080::80 -redir tcp:8888::81 -append \"quiet noautologin home=sda1 opt=sdb1 tce=sdc1\" 2>../../../Reset/MacErrorLog.txt && exit"
     tell application "Terminal"
        activate
        set background color of window 1 to {0,0,0,0}
