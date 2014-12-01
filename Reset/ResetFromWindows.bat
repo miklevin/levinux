d579ad5e12aca4fcb0ae300c7ae08d6bc947281b
Mike Levin <miklevin@gmail.com>
2014-12-01 15:48:56 -0500

    Fixed the Windows reset script and added files

---
+++ Reset/ResetFromWindows.bat
@@ -0,0 +1,11 @@
+cd ..\Pipulate.app\Contents\MacOS\
+move home.qcow ..\..\..\Reset\home-backup.qcow
+del opt.qcow
+del tce.qcow
+del *conflicted*
+del ..\..\..\Reset\*ErrorLog*
+copy home-fresh.qcow home.qcow
+copy opt-fresh.qcow opt.qcow
+copy tce-fresh.qcow tce.qcow
+del stderr.txt
+del stdout.txt
