1302d5885fc5e31345726b8237395a3091fa190c
Joshua Embrey <mrschism@sdf.org>
2015-06-06 20:23:37 -0400

    Initial simple customization system (with example)

---
+++ Reset/Server/Ingredients/install_extras.sh
@@ -0,0 +1,8 @@
+# This grabs and loads extra files from the /Ingredients/Custom folder
+# as specified in the .extras/extras.lst file
+
+while read package; do
+	if [[ $package == \#* ]]; then continue; fi
+	tftp -g -l /mnt/sdc1/tce/optional/$package.tcz -r /Ingredients/Custom/$package.tcz 10.0.2.2
+	sudo -u tc tce-load -i $package > /dev/null
+done <extras.lst
