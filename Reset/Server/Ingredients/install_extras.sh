92d0d674ac47fff1ab61fd628625f66060e95ef9
Joshua Embrey <mrschism@sdf.org>
2015-06-07 02:22:59 -0400

    Fixed extras list and and script to install extras

--- Reset/Server/Ingredients/install_extras.sh
+++ Reset/Server/Ingredients/install_extras.sh
@@ -2,8 +2,8 @@
 # as specified in the .extras/extras.lst file
 
 while read package; do
-	[ "$package" == \#* ] && continue
+	case "$package" in \#*) continue ;; esac
 	[ -z "$package" ] && continue
 	tftp -g -l /mnt/sdc1/tce/optional/$package.tcz -r /Ingredients/Custom/$package.tcz 10.0.2.2
 	sudo -u tc tce-load -i $package > /dev/null
-done <extras.lst
+done < extras.lst
